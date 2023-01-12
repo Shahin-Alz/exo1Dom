5af0e5824565b40c7b2d1adc11db3fadaf1247e6
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-12 14:47:20 +0100

    ajout

---
+++ main.js
@@ -0,0 +1,33 @@
+//let paragraphe = document.getElementsByTagName("p")
+// let paragraphe = document.querySelectorAll("p")
+// console.log(paragraphe);
+
+/////////////
+
+// let h3Elements = document.getElementsByTagName("h3");
+// console.log(h3Elements);
+// let h3Contenu1 = h3Elements[0];
+// console.log( h3Contenu1);
+// let h3Contenu2 = h3Elements[1];
+// console.log(h3Contenu2);
+// let h3Contenu3 = h3Elements[2];
+// console.log(h3Contenu3);
+
+////////////////
+
+// let para = document.getElementsByTagName("p")
+
+// let para2 = para[1]
+// console.log(para2);
+
+//////////////////
+
+// let liste = document.getElementsByTagName("li")
+
+// let listeElement3 = liste[2]
+// console.log(listeElement3);
+
+
+////////////////////////                                              PARTIE2                                              //////////////
+
+
